f931bc8b8835fc82c6764f7e52d9e86abac2c4b9
Lau Jun Xiang <@mqspectrum.com>
2022-01-25 10:37:42 +0800

    Updated to simpler code

--- OOP/LabCode/Week02/Wk02Q2.java
+++ OOP/LabCode/Week02/Wk02Q2.java
@@ -4,18 +4,30 @@ public class Wk02Q2 {
     public static void main(String[] args) {
         float average = 0;
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter three numbers: ");
-        String userInput = input.nextLine();
-        input.close();
+        System.out.print("Enter three numbers: ");
 
-        System.out.print("The average of ");
-        String[] stringArray = userInput.split(" ");
-        for (int i = 0; i <= stringArray.length - 1; i++) {
-            float num = Float.parseFloat(stringArray[i]);
-            System.out.print(num + " ");
-            average += num;
-        }
-        System.out.print("is " + average / stringArray.length);
+        // String userInput = input.nextLine();
+        // input.close();
+
+        // System.out.print("The average of ");
+        // String[] stringArray = userInput.split(" ");
+        // for (int i = 0; i <= stringArray.length - 1; i++) {
+        // float num = Float.parseFloat(stringArray[i]);
+        // System.out.print(num + " ");
+        // average += num;
+        // }
+        // System.out.print("is " + average / stringArray.length);
+
+        float x = 0.0f;
+        x = input.nextFloat();
+        float y = 0.0f;
+        y = input.nextFloat();
+        float z = 0 / 0f;
+        z = input.nextFloat();
+
+        average = (x + y + z) / 3;
+
+        System.out.println("The average of " + x + " " + y + " " + z + " is " + average);
 
     }
 
