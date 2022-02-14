2b03c5076279f447a25647abb5bda32d375cddf3
xiaopang254 <xiaopang254@gmail.com>
2022-02-14 21:28:13 +0800

    Updated for week 6!!

--- OOP/LabCode/Week06/TryCollections.java
+++ OOP/LabCode/Week06/TryCollections.java
@@ -1,6 +1,9 @@
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.List;
 
 public class TryCollections {
     public void printLinkList(LinkedList<Integer> linkedList) {
@@ -10,6 +13,13 @@ public class TryCollections {
             if (i.hasNext() != false) {
                 System.out.print(",");
             }
+        }
+    }
+
+    public void printHashMap(HashMap<Integer, Integer>hashMap) {
+        for (Integer name: hashMap.keySet()){
+            String key = hashMap.get(name).toString();
+            System.out.print(key + " ");
         }
     }
 
@@ -26,6 +36,28 @@ public class TryCollections {
 
     }
 
+    public void hashAddAndSort(HashMap<Integer, Integer> hashMap, int value) {
+
+
+        hashMap.put(hashMap.size(), value);
+        ArrayList<Integer> list = new ArrayList<Integer>(hashMap.values());
+        System.out.print("Before sorted: ");
+        printHashMap(hashMap);
+        
+
+        Collections.sort(list);
+
+        for(int i = 0; i < list.size(); i++)
+        {
+            var test = list.get(i);
+            hashMap.put(i,list.get(i).intValue());
+        }
+
+        System.out.print("\nAfter sorted: ");
+        printHashMap(hashMap);
+
+    }
+
     public void swapValues(LinkedList<Integer> linkedList, int indexOne, int indexTwo) {
 
         System.out.print("\nBefore swapping: ");
@@ -36,6 +68,19 @@ public class TryCollections {
 
     }
 
+    public void hashSwapValues(HashMap<Integer,Integer>hashMap, int valueOneKey, int valueTwoKey) {
+
+        System.out.print("\nBefore swapping: ");
+        printHashMap(hashMap);
+        int valueOne = hashMap.get(valueOneKey);
+        int valueTwo = hashMap.get(valueTwoKey);
+        hashMap.put(valueOneKey, valueTwo);
+        hashMap.put(valueTwoKey, valueOne);
+        System.out.print("\nAfter swapping: ");
+        printHashMap(hashMap);
+
+    }
+
     public int findValue(LinkedList<Integer> linkedList, int searchVal) {
         Collections.sort(linkedList);
         int value = Collections.binarySearch(linkedList, searchVal);
@@ -43,6 +88,19 @@ public class TryCollections {
         if (value < 0) {
             return -1;
         }
+        return value;
+
+    }
+
+    public int hashFindValue(HashMap<Integer,Integer>hashMap, int searchVal) {
+
+        var value = hashMap.get(searchVal);
+
+        if (value == null)
+        {
+            return -1;
+        }
+
         return value;
 
     }
--- OOP/LabCode/Week06/Wk06q1.java
+++ OOP/LabCode/Week06/Wk06q1.java
@@ -1,5 +1,5 @@
 import java.util.Collections;
-import java.util.Iterator;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Random;
 import java.util.Scanner;
@@ -10,12 +10,20 @@ public class Wk06q1 {
 
         TryCollections collect = new TryCollections();
         LinkedList<Integer> linkList = new LinkedList<Integer>();
+        HashMap<Integer, Integer> hashMap = new HashMap<Integer, Integer>();
         linkList.add(1);
         linkList.add(3);
         linkList.add(5);
         linkList.add(7);
         linkList.add(9);
         linkList.add(11);
+
+        hashMap.put(0, 1);
+        hashMap.put(1, 3);
+        hashMap.put(2, 5);
+        hashMap.put(3, 7);
+        hashMap.put(4, 9);
+        hashMap.put(5, 11);
 
         System.out.println("Sorting using Collections!");
         System.out.print("Add a value: ");
@@ -28,7 +36,7 @@ public class Wk06q1 {
         int indexOne = 0;
         int indexTwo = 0;
         collect.printLinkList(linkList);
-        System.out.print("\nInsert for index 1: ");
+        System.out.print("\nInsert two index: ");
         Scanner inputIndex = new Scanner(System.in);
         indexOne = inputIndex.nextInt();
         indexTwo = inputIndex.nextInt();
@@ -43,16 +51,55 @@ public class Wk06q1 {
         int randomValue;
 
         for (int i = min; i <= max; i++) {
-            randomValue = random.nextInt(max - min) + min;
-            bigLL.add(randomValue);
+        randomValue = random.nextInt(max - min) + min;
+        bigLL.add(randomValue);
 
         }
+        System.out.println("\nFind your value using COLLECTIONS BINARY SEARCH!!");
         System.out.println("Enter number between 1000 to 9999: ");
         Scanner rInput = new Scanner(System.in);
         int rValue = rInput.nextInt();
         var rIndex = collect.findValue(bigLL, rValue);
         if (rIndex != -1) {
-            System.out.println("Index found: " + rIndex);
+        System.out.println("Index found: " + rIndex);
+        } else {
+        System.out.println("Index not found.");
+        }
+
+        System.out.println("\n\n\n Using HashMap!!");
+
+        System.out.println("Sorting using Hash Collections!");
+        System.out.print("Add a value: ");
+        Scanner inputSortHash = new Scanner(System.in);
+        collect.hashAddAndSort(hashMap, inputSortHash.nextInt());
+
+        System.out.println("\n\nSwapping using Hash Collections!");
+        System.out.println("Choose which index to swap!");
+        int indexOneHash = 0;
+        int indexTwoHash = 0;
+        collect.printLinkList(linkList);
+        System.out.print("\nInsert two index: ");
+        Scanner inputIndexHash = new Scanner(System.in);
+        indexOne = inputIndexHash.nextInt();
+        indexTwo = inputIndexHash.nextInt();
+        collect.hashSwapValues(hashMap, indexOneHash, indexTwoHash);
+
+        Random randomValuesHash = new Random();
+        HashMap<Integer,Integer>bigHashMap = new HashMap<Integer,Integer>();
+        int randomValue1;
+
+        for (int i = min; i <= max; i++) {
+            randomValue1 = randomValuesHash.nextInt(max - min) + min;
+            bigHashMap.put(randomValue1, randomValue1);
+
+        }
+        System.out.println("\nFind your value using HASH MAP!");
+        System.out.println("Enter number between 1000 to 9999: ");
+        Scanner rHashInput = new Scanner(System.in);
+        int rHashValue = rHashInput.nextInt();
+        var rHashIndex = collect.hashFindValue(bigHashMap, rHashValue);
+        if (rHashIndex != -1) {
+            System.out.println("Index found: " + rHashIndex);
         } else {
             System.out.println("Index not found.");
         }
