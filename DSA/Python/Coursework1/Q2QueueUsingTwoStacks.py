78dcf47c6d2430611e81265c7b5b6be62c704a19
Lau Jun Xiang <@mqspectrum.com>
2022-02-22 16:29:11 +0800

    Optimized QueueUsingTwoStacks solution to hit all test cases

--- DSA/Python/Coursework1/Q2QueueUsingTwoStacks.py
+++ DSA/Python/Coursework1/Q2QueueUsingTwoStacks.py
@@ -1,65 +1,137 @@
 class Stacks:
     def __init__(self):
         self.top = -1
-        self.top2 = -1
         self.data = []
-        self.data2 = []
-
-    def enqueue(self, value):
+    
+    def push(self,value):
         self.data.append(0)
         self.top += 1
         self.data[self.top] = value
+
+    def pop(self):
+        value = self.data[self.top]
+        del self.data[self.top]
+        self.top -=1
+        return value
+
+    def stackCount(self):
+        return len(self.data)
+
+    def stackPrint(self):
+        print(self.data[self.top])
+
+class Queue:
+    def __init__(self):
+        self.stacks1 = Stacks()
+        self.stacks2 = Stacks()
+    
+    def enqueue(self, value):
+        self.stacks1.push(value)
     
     def dequeue(self):
-        if len(self.data) > 0:
-            if len(self.data) == 1:
-                value = self.data[self.top]
-                del self.data[self.top]
-                self.top -= 1
-            else:
-                for i in range(len(self.data)-1,-1,-1):
-                    self.top2 += 1
-                    self.data2.append(self.data[i])
-                    del self.data[i]
-                    self.top -= 1
-
-                value = self.data2[self.top]
-                del self.data2[self.top]
-
-                for i in range(len(self.data2)-1,-1,-1):
-                    self.top += 1
-                    self.data.append(self.data2[i])
-                    del self.data2[i]
-                    self.top2 -= 1
-                return value
-            return value
-        else:
-            return 0
+        if self.stacks1.stackCount() < 1 and self.stacks2.stackCount() < 1:
+            return
         
+        if self.stacks2.stackCount() >= 1:
+            self.stacks2.pop()
+        else:
+            while(True):
+                if self.stacks1.stackCount() == 1 and self.stacks2.stackCount() < 1:
+                    self.stacks1.pop()
+                    break
+                elif self.stacks1.stackCount() >= 1:
+                    self.stacks2.push(self.stacks1.pop())
+                else:
+                    self.stacks2.pop()
+                    break
 
     def printQueue(self):
-        print(self.data[0])
+        if self.stacks1.stackCount() < 1 and self.stacks2.stackCount() < 1:
+            return
+
+        if self.stacks1.stackCount() >= 1 and self.stacks2.stackCount() >= 1:
+            self.stacks2.stackPrint()
+
+        elif self.stacks1.stackCount() < 1 and self.stacks2.stackCount() >= 1:
+            self.stacks2.stackPrint()
+
+        elif self.stacks1.stackCount() == 1 and self.stacks2.stackCount() < 1:
+            self.stacks1.stackPrint()
+
+        elif self.stacks1.stackCount() >= 1 and self.stacks2.stackCount() < 1:
+            while(True):
+                if self.stacks1.stackCount() >= 1:
+                    self.stacks2.push(self.stacks1.pop())
+                else:
+                    self.stacks2.stackPrint()
+                    break
+            
+
+
+
+
+
+# class Queue:
+#     def __init__(self):
+#         self.top = -1
+#         self.top2 = -1
+#         self.data = []
+#         self.data2 = []
+
+#     def enqueue(self, value):
+#         self.data.append(0)
+#         self.top += 1
+#         self.data[self.top] = value
+    
+#     def dequeue(self):
+#         if len(self.data) > 0:
+#             if len(self.data) == 1:
+#                 value = self.data[self.top]
+#                 del self.data[self.top]
+#                 self.top -= 1
+#             else:
+#                 for i in range(len(self.data)-1,-1,-1):
+#                     self.top2 += 1
+#                     self.data2.append(self.data[i])
+#                     del self.data[i]
+#                     self.top -= 1
+
+#                 value = self.data2[self.top]
+#                 del self.data2[self.top]
+
+#                 for i in range(len(self.data2)-1,-1,-1):
+#                     self.top += 1
+#                     self.data.append(self.data2[i])
+#                     del self.data2[i]
+#                     self.top2 -= 1
+#                 return value
+#             return value
+#         else:
+#             return 0
+        
+
+#     def printQueue(self):
+#         print(self.data[0])
 
 
 if __name__ == '__main__':
     q = input()
     inputSize = 0
 
-    stacks = Stacks()
+    queue = Queue()
     while(True):
         if inputSize >= int(q):
             break
-
 
         userInput = input().split()
 
         if (userInput[0] == "1"):
             try:
-                stacks.enqueue(userInput[1])
+                queue.enqueue(userInput[1])
             except:
                 print("Missing 2nd argument.")
         elif (userInput[0] == "2"):
-            stacks.dequeue()
+            queue.dequeue()
         elif (userInput[0] == "3"):
-            stacks.printQueue()
+            queue.printQueue()
         inputSize += 1
